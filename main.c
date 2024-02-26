9cf8b2baeafe7098302325ca5ce8a2e9f53593d6
Chen, Wei-Yu <49446811+WilliamChen1109@users.noreply.github.com>
2024-02-26 14:00:50 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,42 @@
-#include "M480.h"
-#define GPIO_PIN_MAX 16UL;
+#include "M480.h";
+#define GPIO_PIN_MAX 16UL
 
 int32_t main(void){
     // Input Pin Set
-    GPIO_SetMode(PA, BIT0, GPIO_MODE_INPUT);
-    GPIO_SetMode(PA, BIT1, GPIO_MODE_INPUT);
+    GPIO_SetMode(PA, BIT0, GPIO_MODE_INPUT); // SW1
+    GPIO_SetMode(PA, BIT1, GPIO_MODE_INPUT); // SW2
+
+    GPIO_SetMode(PG, BIT2, GPIO_MODE_INPUT); // Up
+    GPIO_SetMode(PG, BIT10, GPIO_MODE_INPUT); //Down
+    GPIO_SetMode(PG, BIT3, GPIO_MODE_INPUT); // Center
 
     // Output Pin Set
-    GPIO_SetMode(PH, BIT6, GPIO_MODE_OUTPUT);
-    GPIO_SetMode(PH, BIT7, GPIO_MODE_OUTPUT);
+    GPIO_SetMode(PH, BIT6, GPIO_MODE_OUTPUT); // LEDR1
+    GPIO_SetMode(PH, BIT7, GPIO_MODE_OUTPUT); // LEDG1
 
     while (1) {
-        PH6 = (PA0 == 0) ? 0 : 1;
-        PH7 = (PA1 == 0) ? 0 : 1;
+        if(PG2 && !PG10){
+            PH6 = 1;
+            PH7 = 0;
+        }
+        else if(!PG2 && PG10){
+            PH6 = 0;
+            PH7 = 1;
+        }
+        else if(!PG2 && !PG10){
+            PH6 = 1;
+            PH7 = 1;
+        }
+        else{
+            PH6 = (PA0 == 0) ? 0 : 1;
+            PH7 = (PA1 == 0) ? 0 : 1;
+        }        
     }    
 }
 
 void GPIO_SetMode(GPIO_T *port, uint32_t u32PinMask, uint32_t u32Mode){
     uint32_t i;
-    for(int i  = 0; i < GPIO_PIN_MAX ; i++){
+    for(i = 0; i < GPIO_PIN_MAX ; i++){
         if((u32PinMask & (lul << i)) == (lul << i)){
             port->MODE = (port->MODE & ~(0x3ul (i << l))) | (u32Mode << (i << l));
         }
