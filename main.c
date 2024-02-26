0e433990ec943169b79fb6f85268db0aee3d167b
Chen, Wei-Yu <49446811+WilliamChen1109@users.noreply.github.com>
2024-02-26 14:56:03 +0800

    Update main.c

--- main.c
+++ main.c
@@ -13,23 +13,24 @@
         // Output Pin Set
         GPIO_SetMode(PH, BIT6, GPIO_MODE_OUTPUT); // LEDR1
         GPIO_SetMode(PH, BIT7, GPIO_MODE_OUTPUT); // LEDG1
-
+        
+        // While Loop
         while (1) {
-            if(PG2 && !PC10){
-                PH6 = 1;
-                PH7 = 0;
+            if(PG2 && !PC10){ // Up
+                PH6 = 1; // LEDR1 OFF
+                PH7 = 0; // LEDG1 ON
             }
-            else if(!PG2 && PC10){
-                PH6 = 0;
-                PH7 = 1;
+            else if(!PG2 && PC10){ // Down
+                PH6 = 0; // LEDR1 ON
+                PH7 = 1; // LEDG1 OFF
             }
-            else if(!PG3){
-                PH6 = 0;
-                PH7 = 0;
+            else if(!PG3){  // Center
+                PH6 = 0; // LEDR1 ON
+                PH7 = 0; // LEDG1 ON
             } 
             else{
-                PH6 = (PA0 == 0) ? 0 : 1;
-                PH7 = (PA1 == 0) ? 0 : 1;
+                PH6 = (PA0 == 0) ? 0 : 1; // LEDR1 on when SW1 Pressed
+                PH7 = (PA1 == 0) ? 0 : 1; // LEDG1 on when SW2 Pressed
             }        
         }    
     }
