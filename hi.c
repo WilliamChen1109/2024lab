a93fb2d90ba541cec13809948466d6e8c8432efc
Chen, Wei-Yu <49446811+WilliamChen1109@users.noreply.github.com>
2024-03-11 15:15:53 +0800

    Create hi.c

---
+++ hi.c
@@ -0,0 +1,11 @@
+/* local variable define */
+				char clock_buf[20];
+			
+				joystick_pins.UP = SW_UP;	//SW_UP
+                joystick_pins.DOWN = SW_DOWN;	//SW_DOWN
+                joystick_pins.CTR = SW_CTR;	//SW_CTR;
+				//SW_DOWN;
+				//SW_CTR;
+			
+				/* Joystick CTL */
+                JoyStick(joystick_pins);
